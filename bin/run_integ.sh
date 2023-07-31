0d7404d873356ae6d0c4ffef7eafc49ddde6b3c6
George Dyrrachitis <giorgos.dyrrahitis@gmail.com>
2023-07-31 01:00:23 +0100

    fixed integ testing

--- bin/run_integ.sh
+++ bin/run_integ.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker compose -f ./docker-compose.integ.yml down
-docker compose -f ./docker-compose.integ.yml up --force-recreate --build
+docker compose -f ./docker-compose.integ.yml up --force-recreate --build --remove-orphans --exit-code-from "myrepo_manager_integ"
